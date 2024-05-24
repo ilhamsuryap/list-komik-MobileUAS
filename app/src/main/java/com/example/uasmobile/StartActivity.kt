11018ec9de6a57d098e7eb96ceebf8fda029be7d
ilhamsuryap <ilhamsurya123123@gmail.com>
2024-05-24 14:36:16 +0700

    membuat tombol get started

---
+++ app/src/main/java/com/example/uasmobile/StartActivity.kt
@@ -0,0 +1,22 @@
+package com.example.uasmobile
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.uasmobile.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener{
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+} }
+}
